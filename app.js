f3c0a88c9744bb964157a8805c587c28a0ba69c0
Kevin Lu <kevie.lu8@gmail.com>
2022-05-28 19:35:53 -0600

    backup copy in case we kill something

--- app.js
+++ app.js
@@ -1,23 +1,79 @@
-const	DATABASE_URL = "postgresql://sql:WoW0eh5qWUanCjAHr8E9yw@free-tier4.aws-us-west-2.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full&options=--cluster%3Dchic-sphinx-3020";
-
 const PORT = 8080;
-
+require("dotenv").config();
+const DATABASE_URL = process.env.DATABASE_URL;
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const path = require("path");
-const url = require('url')
+const ws = require("ws");
 
 // CockroachDB
 const { Client } = require("pg");
+const { freemem } = require("os");
 const client = new Client(DATABASE_URL);
 
 client.connect();
+
+// Websocket
+var sockets = [];
+
+var queue = null;
+
+var wss = new ws.Server({ port: 8081 });
+
+wss.on("connection", (s) => {
+	s.on("message", (data) => {
+		// Initial handshake
+		if (data[0] == "\x00") {
+			s.token = data.subarray(1); // handshake
+			if (queue) {
+				s.peer = queue;
+				queue.peer = s;
+				s.send("\x00" + client.query(`SELECT username FROM tokens WHERE token='${queue.token}'`));
+				queue.send("\x00" + client.query(`SELECT username FROM tokens WHERE token='${s.token}'`));
+				queue = null;
+			} else {
+				queue = s;
+			}
+		} else if (data == "\x01") {
+			s.isAlive = true;
+		} else {
+			s.peer.send(data);
+		}
+	});
+	s.on("close", () => {
+		if (s.peer) {
+			s.peer.peer = null;
+			s.peer.terminate();
+		}
+	});
+	s.heartbeat = setInterval(() => {
+		if (s.isAlive == false) {
+			s.peer.peer = null;
+			s.peer.terminate();
+			clearInterval(s.heartbeat);
+			return s.terminate();
+		}
+		s.isAlive = false;
+		s.send("\x01");
+	}, 5000);
+});
+
+function generateToken() {
+	const tokenChars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	let token = "";
+	for (let i = 0; i < 16; i++) {
+		token += tokenChars[Math.floor(Math.random() * tokenChars.length)];
+	}
+	return token;
+}
 
 // setting stuff up
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
 	res.sendFile(path.join(__dirname, "/public/html/index.html"));
@@ -31,12 +87,23 @@ app.get("/register", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/html/register.html"));
 });
 
+app.get("/logout", (req, res) => {
+	client.query(`DELETE FROM tokens WHERE token='${req.cookies['token']}'`);
+	res.clearCookie("token");
+	res.redirect("/");
+});
+
 app.get("/about", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/html/about.html"));
 })
 
-app.get("/chat", (req, res) => {
-	res.sendFile(path.join(__dirname, "/public/html/chat.html"));
+app.get("/chat", async (req, res) => {
+	if ((await client.query(`SELECT * FROM tokens WHERE token='${req.cookies['token']}'`)).rowCount) {
+		res.sendFile(path.join(__dirname, "/public/html/chat.html"));
+	} else {
+		res.redirect("/login?err=1")
+	}
+	// res.sendFile(path.join(__dirname, "/public/html/chat.html"));
 });
 
 app.get("*", (req, res) => {
@@ -45,24 +112,37 @@ app.get("*", (req, res) => {
 
 /* ------------------------------ API ------------------------------ */
 app.post("/api/register", async (req, res) => {
-	console.log(req.body);
 	if ((await client.query(`SELECT username FROM users WHERE username='${req.body['username']}';`)).rowCount) {
-		res.redirect("/register?err");
+		res.redirect("/register?err=0");
 	} else {
 		client.query(`INSERT INTO users (username, password) VALUES ('${req.body['username']}', '${req.body['password']}');`);
-		res.cookie = ""
+		let token;
+		do {
+			token = generateToken();
+		} while ((await client.query(`SELECT * FROM tokens WHERE 	token='${token}'`)).rowCount)
+		client.query(`INSERT INTO tokens (token, username) VALUES ('${token}', '${req.body['username']}')`);
+		res.cookie("token", token, { maxAge: 86400000 });
 		res.redirect("/chat");
 	}
 });
 
-app.post("/api/login", (req, res) => {
-	console.log(req.body);
-	res.redirect("/chat");
-});
-
-app.post("/api/logout", (req, res) => {
-	console.log(req.body);
-	res.redirect("/");
+app.post("/api/login", async (req, res) => {
+	if (((await client.query(`SELECT username FROM users WHERE username='${req.body['username']}';`)).rowCount)) {
+		if ((await client.query(`SELECT * FROM users WHERE (username='${req.body['username']}' AND password='${req.body['password']}')`)).rowCount) {
+			await client.query(`DELETE FROM tokens WHERE username='${req.body['username']}'`);
+			let token;
+			do {
+				token = generateToken();
+			} while ((await client.query(`SELECT * FROM tokens WHERE token='${token}'`)).rowCount)
+			client.query(`INSERT INTO tokens (token, username) VALUES ('${token}', '${req.body['username']}')`);
+			res.cookie("token", token, { maxAge: 86400000 });
+			res.redirect("/chat");
+		} else {
+			res.redirect(`/login?username=${req.body['username']}&err=0`);
+		}
+	} else {
+		res.redirect("/register?err=1");
+	}
 });
 
 app.listen(PORT);
--- public/js/checkLogin.js
+++ public/js/checkLogin.js
@@ -1,6 +1,7 @@
 if (document.cookie.length > 0) {
 	console.log(document.cookie);
 	document.getElementById("btn-login").innerHTML = "Logout";
+	console.log(document.getElementById("btn-login").innerHTML);
 	document.getElementById("btn-register").remove();
 	document.getElementById("btn-login").onclick = function() {
 		window.location.href = "/logout";
